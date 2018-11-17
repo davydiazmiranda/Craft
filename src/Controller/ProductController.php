ef357061e3a6e30036d366cf4f76026fa4edd71b
ledevpole <ledevpole@gmail.com>
2018-11-17 18:21:54 +0100

    done basics + uploadfile basic management

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+ */
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/", name="product_index", methods="GET")
+     */
+    public function index(ProductRepository $productRepository): Response
+    {
+        return $this->render('product/index.html.twig', ['products' => $productRepository->findAll()]);
+    }
+
+    /**
+     * @Route("/new", name="product_new", methods="GET|POST")
+     */
+    public function new(Request $request): Response
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($product);
+            $em->flush();
+            //persist and flush then take it back now that i have an id and upload from tmp
+            //spanish power ^^
+            $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+            $extension_upload = strtolower(  substr(  strrchr($_FILES['product']['name']["img"], '.')  ,1)  );
+            $host_name = "http://localhost:8000/";
+
+            move_uploaded_file($product->getImg(), 'assets/img/products/'. $product->getId().".{$extension_upload}");
+            $product->setImg($host_name.'assets/img/products/'. $product->getId().".{$extension_upload}");
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_index');
+        }
+
+        return $this->render('product/new.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="product_show", methods="GET")
+     */
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', ['product' => $product]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="product_edit", methods="GET|POST")
+     */
+    public function edit(Request $request, Product $product): Response
+    {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+
+            //persist and flush then take it back now that i have an id and upload from tmp
+            //spanish power ^^
+            $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png' );
+            $extension_upload = strtolower(  substr(  strrchr($_FILES['product']['name']["img"], '.')  ,1)  );
+            $host_name = "http://localhost:8000/";
+
+            move_uploaded_file($product->getImg(), 'assets/img/products/'. $product->getId().".{$extension_upload}");
+            $product->setImg($host_name.'assets/img/products/'. $product->getId().".{$extension_upload}");
+            $em->persist($product);
+            $em->flush();
+
+            return $this->redirectToRoute('product_index', ['id' => $product->getId()]);
+        }
+
+        return $this->render('product/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="product_delete", methods="DELETE")
+     */
+    public function delete(Request $request, Product $product): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($product);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('product_index');
+    }
+}
